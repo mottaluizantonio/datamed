1cd07cea6924f7f86131c64e0d7e901270bb1088
Luiz Antonio Motta <mottaluizantonio@gmail.com>
2022-03-21 20:13:04 -0300

    feat: header, dados do paciente com calculo da idade, botao voltar

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,93 @@
+import { useEffect, useState } from "react";
+import {
+  Button,
+  ColumnBox,
+  Container,
+  Content,
+  Header,
+  RowBox,
+  Text,
+  Title,
+} from "../../styles/theme";
 import {} from "./style";
+import logo from "../../img/logo.svg";
+import { ModalConsulta } from "../../components/ModalConsulta";
+import { useParams } from "react-router-dom";
+import { useDetails } from "../../providers/Details";
+import { HeaderBottomLine } from "./style";
+import { useHistory } from "react-router-dom";
+import { useLogin } from "../../providers/Login";
+
 export const Details = () => {
-    return <></>;
+  const { cpf } = useParams();
+  const { paciente, setPaciente, selectPaciente, getDadosPaciente } =
+    useDetails();
+  const { idLogado } = useLogin();
+
+  useEffect(() => {
+    setPaciente(selectPaciente(cpf), []);
+  }, []);
+
+  const calculate_age = (paciente) => {
+    const data_nascimento = paciente.data_nascimento.split("/");
+    const dia_nascimento = Number(data_nascimento[0]);
+    const mes_nascimento = Number(data_nascimento[1]);
+    const ano_nascimento = Number(data_nascimento[2]);
+
+    const hoje = new Date();
+    const dia_atual = hoje.getDate();
+    const mes_atual = hoje.getMonth() + 1;
+    const ano_atual = hoje.getFullYear();
+
+    let anos = ano_atual - ano_nascimento;
+
+    if (
+      mes_atual < mes_nascimento ||
+      (mes_atual === mes_nascimento && dia_atual < dia_nascimento)
+    ) {
+      anos--;
+    }
+
+    return anos < 0 ? 0 : anos;
+  };
+
+  const history = useHistory();
+
+  const handleVoltar = () => {
+    history.push(`/dashboard/${idLogado}`);
+  };
+
+  return (
+    <>
+      <Container>
+        <Header>
+          <img src={logo} alt="Datamed logo" />
+          <RowBox width="auto">
+            <Button>Histórico familiar aqui</Button>
+            <Button>Nova consulta</Button>
+            <Button onClick={handleVoltar}>voltar</Button>
+          </RowBox>
+        </Header>
+
+        <HeaderBottomLine />
+
+        <Header>
+          <ColumnBox width="auto">
+            <Title>Nome paciente: {paciente.nome}</Title>
+            <Text>Idade: {calculate_age(paciente)}</Text>
+          </ColumnBox>
+          <ColumnBox width="auto">
+            <Text>Profissão: {paciente.profissao}</Text>
+            <Text>Fumante: {!!paciente.status_fumante ? "sim" : "não"}</Text>
+          </ColumnBox>
+        </Header>
+
+        <Content>
+          <ColumnBox>Histórico</ColumnBox>
+          <ColumnBox>Consultas</ColumnBox>
+        </Content>
+      </Container>
+      <ModalConsulta></ModalConsulta>
+    </>
+  );
 };
--- src/providers/Details/index.js
+++ src/providers/Details/index.js
@@ -1,57 +1,105 @@
-import { useEffect, useState } from "react";
-import { createContext, useContext } from "react";
-const DetailsContext = createContext([]);
+import { createContext, useContext, useState } from "react";
+
+const DetailsContext = createContext();
+
 export const DetailsProvider = ({ children }) => {
-    const [consultas, setConsultas] = useState([]);
-    const [diagnosticos, setDiagnosticos] = useState([]);
-    const [historicoFamiliar, setHistoricoFamiliar] = useState([]);
-    const [paciente, setPaciente] = useState({});
-    useEffect(() => getDadosPaciente(), [paciente]);
-    const selectPaciente = (cpf = undefined) => {
-        //Mock abaixo
-        let pacienteSelecionado = cpf !== undefined && {
-            userId: 2,
-            nome: "Vinicius",
-            email: "vini@mail.com",
-            data_nascimento: "18/02/1987",
-            cpf: "67656456",
-            profissao: "Médico",
-            celular: "45496498",
-            status_fumante: 0,
-            tipo: "paciente",
-            crm: "",
+  const [consultas, setConsultas] = useState([]);
+  const [diagnosticos, setDiagnosticos] = useState([]);
+  const [historicoFamiliar, setHistoricoFamiliar] = useState([]);
+  const [paciente, setPaciente] = useState([]);
+
+  const selectPaciente = (cpf = undefined) => {
+    //Mock abaixo
+    let pacienteSelecionado = cpf !== undefined && {
+      userId: 2,
+      nome: "Vinicius",
+      email: "vini@mail.com",
+      data_nascimento: "18/02/1987",
+      cpf: "67656456",
+      profissao: "Médico",
+      celular: "45496498",
+      status_fumante: 0,
+      tipo: "paciente",
+      crm: "",
+      id: 2,
+    };
+    //Mock acima
+    // setPaciente(pacienteSelecionado);
+    // console.log("pacienteSelecionado", pacienteSelecionado);
+    return pacienteSelecionado;
+  };
+
+  const getDadosPaciente = () => {
+    //Mocks abaixo
+    let listaDiagnosticos = !!paciente?.id
+      ? [
+          {
+            id: 1,
+            idConsulta: 1,
+            tipo: "Doença Cronica",
+            descricao: "Dermatite atópica",
+          },
+          {
             id: 2,
-        };
-        //Mock acima
-        setPaciente(pacienteSelecionado);
-    };
-    const getDadosPaciente = () => {
-        //Mocks abaixo
-        let listaDiagnosticos = !!paciente?.id
-            ? [
-                  { id: 1, idConsulta: 1, tipo: "Doença Cronica", descricao: "Dermatite atópica" },
-                  { id: 2, idConsulta: 1, tipo: "Medicamento constante", descricao: "Hidratante - Nutrel Profuse" },
-                  { id: 2, idConsulta: 2, tipo: "Doença Cronica", descricao: "Ceratocone" },
-              ]
-            : [];
-        let listaHistoricoFamiliar = !!paciente?.id
-            ? [
-                  { id: 1, idPaciente: 2, grau: "1º", parentesco: "pai", nome: "Francisco Alves Pereira", doenca: "Ceratocone" },
-                  { id: 2, idPaciente: 2, grau: "1º", parentesco: "mae", nome: "Elsa Rocha Bento", doenca: "Dermatite atópica" },
-              ]
-            : [];
-        let listaConsultas = !!paciente?.id
-            ? [
-                  { id: 1, crm_medico: "79461325855", desc: "O paciente está com dermatite atópica" },
-                  { id: 2, crm_medico: "79461325855", desc: "o pacioente tem ceratocone" },
-              ]
-            : [];
-        //Mocks acima
-        setDiagnosticos(listaDiagnosticos);
-        setHistoricoFamiliar(listaHistoricoFamiliar);
-        setConsultas(listaConsultas);
-    };
-    return <DetailsContext.Provider value={{ paciente, selectPaciente, consultas, diagnosticos, historicoFamiliar }}>{children}</DetailsContext.Provider>;
+            idConsulta: 1,
+            tipo: "Medicamento constante",
+            descricao: "Hidratante - Nutrel Profuse",
+          },
+          {
+            id: 2,
+            idConsulta: 2,
+            tipo: "Doença Cronica",
+            descricao: "Ceratocone",
+          },
+        ]
+      : [];
+    let listaHistoricoFamiliar = !!paciente?.id
+      ? [
+          {
+            id: 1,
+            idPaciente: 2,
+            grau: "1º",
+            parentesco: "pai",
+            nome: "Francisco Alves Pereira",
+            doenca: "Ceratocone",
+          },
+          {
+            id: 2,
+            idPaciente: 2,
+            grau: "1º",
+            parentesco: "mae",
+            nome: "Elsa Rocha Bento",
+            doenca: "Dermatite atópica",
+          },
+        ]
+      : [];
+    let listaConsultas = !!paciente?.id
+      ? [
+          {
+            id: 1,
+            crm_medico: "79461325855",
+            desc: "O paciente está com dermatite atópica",
+          },
+          {
+            id: 2,
+            crm_medico: "79461325855",
+            desc: "o pacioente tem ceratocone",
+          },
+        ]
+      : [];
+    //Mocks acima
+    setDiagnosticos(listaDiagnosticos);
+    setHistoricoFamiliar(listaHistoricoFamiliar);
+    setConsultas(listaConsultas);
+  };
+
+  return (
+    <DetailsContext.Provider
+      value={{ paciente, setPaciente, selectPaciente, getDadosPaciente }}
+    >
+      {children}
+    </DetailsContext.Provider>
+  );
 };
 
 export const useDetails = () => useContext(DetailsContext);
