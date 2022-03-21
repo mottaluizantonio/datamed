5c9d6a5eff47f4c4b43794b3ef273654ba4c95a8
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-03-21 15:44:02 -0300

    feat: Header e grid de pacientes

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,53 @@
-import {} from "./style";
-import { Button } from "../../styles/theme";
-import { useLogin } from "../../providers/Login";
+import {
+	Button,
+	Container,
+	Content,
+	Header,
+	RowBox,
+	Title,
+} from '../../styles/theme';
+import { useLogin } from '../../providers/Login';
+import logo from '../../img/logo.svg';
+import { useDashboard } from '../../providers/Dashboard';
+import { Datagrid } from '../../styles/Datagrid';
+import { useParams } from 'react-router-dom';
 export const Dashboard = () => {
-    const { logout } = useLogin();
-    return (
-        <div>
-            <Button onClick={() => logout("/")}>Logout</Button>
-        </div>
-    );
+	const { pacientes, setMedico } = useDashboard();
+	const { logout } = useLogin();
+    const { id } = useParams()
+    setMedico(40)
+	let gridColumns = [
+		{
+			label: 'Nome do Paciente',
+			key: 'nome',
+			type: 'text',
+			width: 100,
+		},{
+			label: 'Ação',
+			type: 'button',
+			// onclick: onClickTeste,
+			key: 'Detalhes',
+			width: 100,
+		},
+	];
+	return (
+		<Container>
+			<Header>
+				<RowBox width='25%'>
+					<img src={logo} alt='Datamed' />
+				</RowBox>
+				<Button onClick={() => logout('/')}>Sair</Button>
+			</Header>
+			<RowBox style={{ padding: '20px 0 0 0' }}>
+				<Title>Bem vindo, Dr. Strange!</Title>
+			</RowBox>
+			<Content>
+				<Datagrid
+					title='Pacientes'
+					columns={gridColumns}
+					data={pacientes}
+				/>
+			</Content>
+		</Container>
+	);
 };
