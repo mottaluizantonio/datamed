9f4abc8fc3144c4fe9c0d43bbd7d1f360d05273c
Luiz Antonio Motta <mottaluizantonio@gmail.com>
2022-03-23 14:24:04 -0300

    build: modal do Historico Familiar construido

--- src/components/ModalDetalhesConsulta/index.jsx
+++ src/components/ModalDetalhesConsulta/index.jsx
@@ -1,19 +1,31 @@
 import { button, rowBar } from "./style";
-import { Button, ColumnBox, ModalBox, RowBox, Text, Input, Select } from "../../theme";
+import {
+  Button,
+  ColumnBox,
+  ModalBox,
+  RowBox,
+  Text,
+  Input,
+  Select,
+} from "../../theme";
 import { useModal } from "../../providers/Modal";
 export const ModalDetalhesConsulta = () => {
-    const { Switch, stateModalDetalhesConsulta } = useModal();
+  const { Switch, stateModalDetalhesConsulta } = useModal();
 
-    return (
-        <ModalBox hidden={stateModalDetalhesConsulta}>
-            <ColumnBox bgColor="grey0" width="600px">
-                <RowBox bgColor="primary" style={rowBar}>
-                    <Text>DETALHES CONSULTA</Text>
-                    <Button bgColor="negative" style={button} onClick={() => Switch("ModalDetalhesConsulta")}>
-                        x
-                    </Button>
-                </RowBox>
-            </ColumnBox>
-        </ModalBox>
-    );
+  return (
+    <ModalBox hidden={stateModalDetalhesConsulta}>
+      <ColumnBox bgColor="grey0" width="600px">
+        <RowBox bgColor="primary" style={rowBar}>
+          <Text>DETALHES CONSULTA</Text>
+          <Button
+            bgColor="negative"
+            style={button}
+            onClick={() => Switch("ModalDetalhesConsulta")}
+          >
+            x
+          </Button>
+        </RowBox>
+      </ColumnBox>
+    </ModalBox>
+  );
 };
